57b8a807042ea513f651a8dcd33fd0542ec14af4
arthurigm1 <arthurmartins00@hotmail.com>
2025-11-07 14:09:46 -0300

    teste prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   // Base URL do backend; ajuste via file replacements no build se necessário
-  apiBaseUrl: 'http://localhost:3010'
+  apiBaseUrl: 'https://backend-production-d59b.up.railway.app'
 };
